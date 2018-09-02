af848fde904f505246557b607fa343cc60d928b9
wanghuiwhwx <wanghui_whwx@sina.com>
2018-09-02 19:23:59 +0800

    测试代码上传

--- src/main/java/cn/tianjin/unifiedfee/ot/OtApplication.java
+++ src/main/java/cn/tianjin/unifiedfee/ot/OtApplication.java
@@ -7,16 +7,13 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
 @EnableFeignClients({"cn.taiji","cn.tianjin.unifiedfee"})
 @ComponentScan({"cn.taiji","cn.tianjin.unifiedfee"})
 public class OtApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OtApplication.class, args);
 	}
-
 }
